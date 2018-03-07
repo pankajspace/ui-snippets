55484308d4107529d5013ae5e4cd74a77d15a4d4
pankajspace <pankaj.corp@gmail.com>
2018-03-07 11:28:28 +0530

    two way data binding implemented for multiple inputs.

--- js/bindings/two-way-binding.js
+++ js/bindings/two-way-binding.js
@@ -1,66 +1,37 @@
 "use strict";
 
-// var input = document.querySelector("input");
-// var objData = {
-//   dataValue: input.getAttribute("data-value"),
-//   prevInputValue: input.getAttribute("data-value")
-// };
-// //changing the value of input to that provided in attribute for first time.
-// input.value = objData.dataValue;
-
-// //watching the dataValue changes, if changed changing the input value.
-// setInterval(function() {
-//   if (objData.dataValue !== objData.prevInputValue) {
-//     input.value = objData.dataValue;
-//   }
-// }, 0);
-
-// input.addEventListener("keyup", function(event) {
-//   objData.prevInputValue = event.target.value;
-//   objData.dataValue = event.target.value;
-// });
-
-
-
-
-
-
-
-
-
 var inputs = document.querySelectorAll("input");
+var objArray = [];
 
-console.log(inputs);
-
+//adding event listeners to all inputs and associating objData for storing data related to that input.
 inputs.forEach(function(input) {
+  input.value = input.getAttribute("data-value");
+  var objData = {
+    dataValue: input.getAttribute("data-value"),
+    set dataValue(val) {
+      this.currentDataValue = val;
+      input.value = val;
+    },
+    get dataValue() {
+      return this.dataValue;
+    },
+    currentDataValue: null
+  };
   input.addEventListener("keyup", function(event) {
     objData.dataValue = event.target.value;
-    console.log(objData);
+    input.nextElementSibling.innerHTML = objData.currentDataValue;
+    console.log(objData, input.next);
   });
+  objArray.push({ input: input, data: objData });
 });
 
-var input = document.querySelector("input");
-var objData = {
-  dataValue: input.getAttribute("data-value"),
-  set dataValue(val) {
-    this.currentDataValue = val;
-    input.value = val;
-  },
-  get dataValue() {
-    return this.dataValue;
-  },
-  currentDataValue: null
-};
-//changing the value of input to that provided in attribute for first time.
-input.value = input.getAttribute("data-value");
-
-input.addEventListener("keyup", function(event) {
-  objData.dataValue = event.target.value;
-  console.log(objData);
-});
-
+//setting data for each input randomly.
 var button = document.querySelector("button");
 button.addEventListener("click", function(event) {
-  objData.dataValue = Math.floor(Math.random() * 10000);
-  console.log(objData);
+  objArray.forEach(function(obj) {
+    console.log(obj);
+    var data = Math.floor(Math.random() * 10000);
+    obj.data.dataValue = data;
+    obj.input.nextElementSibling.innerHTML = data;
+  });
 });
