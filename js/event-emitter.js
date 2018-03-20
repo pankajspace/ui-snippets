35d9102d3654a58ee06aea65b4a5a3c3a3ef669a
pankajspace <pankaj.corp@gmail.com>
2018-03-20 11:35:53 +0530

    created custom event emitter.

---
+++ js/event-emitter.js
@@ -0,0 +1,30 @@
+function EventEmitter() {
+  this.events = {};
+}
+
+EventEmitter.prototype.on = function(type, listener) {
+  this.events[type] = this.events[type] || [];
+  this.events[type].push(listener);
+  //   console.log(this.events);
+};
+
+EventEmitter.prototype.emit = function(type, data) {
+  if (this.events[type]) {
+    this.events[type].forEach(listener => {
+      listener(data);
+    });
+  }
+};
+
+var eventEmitter = new EventEmitter();
+
+eventEmitter.on("click", function(data) {
+  console.log("clicked 1", data);
+});
+
+eventEmitter.on("click", function(data) {
+  console.log("clicked 2", data);
+});
+
+eventEmitter.emit("click", { name: "pankaj" });
+eventEmitter.emit("click", { name: "kunal" });
