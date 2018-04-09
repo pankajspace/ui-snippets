32837c55ed0dc8d9ceef5bedd99ec64b1b228871
Pankaj Wakchaure <pankaj.corp@gmail.com>
2018-04-09 20:43:40 +0530

    singleton pattern added.

---
+++ js/patterns/singleton.js
@@ -0,0 +1,35 @@
+var mySingleton = (function () {
+
+    var instance;
+
+    function init() {
+        function privateMethod(params) {
+            console.log("privateMethod");
+        }
+        var privateVariable = "privateVariable"
+        var privateRandomNumber = Math.random();
+        return {
+            publicMethod: function (params) {
+                console.log("publicMethod");
+            },
+            publicProperty: "publicProperty",
+            getRandomNumber: function (params) {
+                return privateRandomNumber;
+            }
+        }
+    }
+
+    return {
+        getInstance: function (params) {
+            if (!instance) {
+                instance = init();
+            }
+            return instance;
+        }
+    }
+
+})();
+
+var single1 = mySingleton.getInstance();
+var single2 = mySingleton.getInstance();
+console.log(single1.getRandomNumber() === single2.getRandomNumber());
